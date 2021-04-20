8cda6d9aa3fb8fb562738897237fd773d5df6486
krisj <krisjanis.spridis@gmail.com>
2021-04-20 12:52:24 +0200

    Anwendung kann nun mittels gradlew bootRun gestartet werden

---
+++ src/main/java/HTW/WebTechProjectSoSe2021/WebTechProjectSoSe2021/HelloController.java
@@ -0,0 +1,17 @@
+package HTW.WebTechProjectSoSe2021.WebTechProjectSoSe2021;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+
+}
+
--- src/main/java/HTW/WebTechProjectSoSe2021/WebTechProjectSoSe2021/WebTechProjectSoSe2021Application.java
+++ src/main/java/HTW/WebTechProjectSoSe2021/WebTechProjectSoSe2021/WebTechProjectSoSe2021Application.java
@@ -1,14 +1,39 @@
 package HTW.WebTechProjectSoSe2021.WebTechProjectSoSe2021;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class WebTechProjectSoSe2021Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebTechProjectSoSe2021Application.class, args);
-		System.out.println("Hello World");
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 }
+
+
+
+
+
