a02ae06c812e6b6698a824a5500304309b3c1c91
krisj <krisjanis.spridis@gmail.com>
2021-05-06 22:01:14 +0200

    erster versuch html template zu kreieren

--- src/main/java/HTW/WebTechProjectSoSe2021/WebTechProjectSoSe2021/ShoppingListController.java
+++ src/main/java/HTW/WebTechProjectSoSe2021/WebTechProjectSoSe2021/ShoppingListController.java
@@ -1,9 +1,11 @@
 package HTW.WebTechProjectSoSe2021.WebTechProjectSoSe2021;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -18,21 +20,23 @@ public class ShoppingListController {
     @Autowired
     private ShoppingListService shoppingListService;
 
-    @GetMapping("/shoppinglists")
-    public List<ShoppingListEntity> allShoppingLists() {
+//    @GetMapping("/shoppinglists")
+//    public List<ShoppingListEntity> allShoppingLists() {
+//
+//        return ShoppingListService.allShoppingLists();
+//    }
 
-        return ShoppingListService.allShoppingLists();
+    @PostMapping("/add-list")
+    public String addListPage(@ModelAttribute ShoppingListEntity shoppingList, Model model) {
+        ShoppingListService.saveList(shoppingList);
+        //model.addAttribute("shoppingList", shoppingList);
+        return "list-added";
     }
 
-    @PostMapping("/addlist?listname?author")
-    public String createList(
-            @PathVariable("listname") String listname,
-            @PathVariable("author") String author
-            //@PathVariable("listitems") List<String>
-    ) {
-        ShoppingListService.createList(listname, author);
-        String s = "Die Liste " + listname + " von " + author + " wurde hinzugefügt!";
-        return s;
+    @PostMapping("/list-added")
+    public String addListPage(Model model) {
+
+        return "List added!";
     }
 }
 
--- src/main/java/HTW/WebTechProjectSoSe2021/WebTechProjectSoSe2021/ShoppingListService.java
+++ src/main/java/HTW/WebTechProjectSoSe2021/WebTechProjectSoSe2021/ShoppingListService.java
@@ -2,27 +2,16 @@ package HTW.WebTechProjectSoSe2021.WebTechProjectSoSe2021;
 
 import org.springframework.stereotype.Component;
 
-import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
-
 @Component
 public class ShoppingListService {
-
-    @OneToMany
-    private final List<ShoppingListEntity> SHOPPING_LIST_ENTITIES = new ArrayList<ShoppingListEntity>();
 
     public ShoppingListService() {
     }
 
-    public static List<ShoppingListEntity> allShoppingLists() {
-        return SHOPPING_LIST_ENTITIES;
-    }
 
-    public static void createList(String listName, String author) {
+    //saves list to DB
+    public static void saveList(ShoppingListEntity shoppinglist) {
 
-        ShoppingListEntity shoppinglist = new ShoppingListEntity(listName, author);
-        SHOPPING_LIST_ENTITIES.add(shoppinglist);
 
     }
 
