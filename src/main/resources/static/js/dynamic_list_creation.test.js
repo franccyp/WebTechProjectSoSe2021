aa3cd9152dbacb74adadc6c9a95ea66b28faa804
krisj <krisjanis.spridis@gmail.com>
2021-06-24 17:18:12 +0200

    first front end js test works

--- src/main/resources/static/js/dynamic_list_creation.test.js
+++ src/main/resources/static/js/dynamic_list_creation.test.js
@@ -1,11 +1,10 @@
-import {mount} from '@vue?test-utils'
-import dynamic_list_creation from './dynamic_list_creation.js';
+import {mount} from '@vue/test-utils'
+import dynamicListCreation from './dynamic_list_creation';
 
 global.axios = require('axios');
 global.axios = require('sweetalert2');
 
 test('load template', () => {
-    const wrapper = mount(dynamic_list_creation)
-
-    expect(wrapper.text()).toContain('Add a new Shopping List!')
+    const wrapper = mount(dynamicListCreation);
+    expect(wrapper.text()).toContain('Add a new Shopping List!');
 })
